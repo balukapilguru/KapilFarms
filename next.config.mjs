d948324d0342f21f4d09a92c64c4e7f4a7f52504
balukapilguru <balakrishna.n@teksacademy.com>
2024-07-18 15:45:26 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,22 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     output: 'export',
+//     reactStrictMode: true,
+// };
+
+// export default nextConfig;
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
     reactStrictMode: true,
+    trailingSlash: true, // Ensures all paths end with a trailing slash
+    images: {
+        unoptimized: true, // Disables image optimization for static exports
+    },
+    // Optional: If you use basePath, you need to configure it
+    // basePath: '/your-base-path',
 };
 
 export default nextConfig;
